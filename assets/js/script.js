a2ffa4e6fd587244a4146abf3c2a8d422172d6b5
David Greenhouse <dgreenhouse96@gmail.com>
2022-09-01 16:50:30 -0500

    updated js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,24 @@
+
 function getHeaderDate() {
     var currentHeaderDate = moment().format('dddd, MMMM Do');
     $("#currentDay").text(currentHeaderDate);
 }
-
 var momentTime = moment().startOf("day").add(7, "hour");
 var hour = moment().format("HH");
 
+for(var i = 8; i < 18; i++) {
+    var timeSlot = momentTime.add(1, "hour").format("HH:mm A");
+    var currentState;
+
+    if (hour == i) {
+        currentState = 'present';
+    } else if (hour < i) {
+        currentState = 'future';
+    } else if (hour > i) {
+        currentState = 'past';
+    }
+
+};
 
 /* 
 getHeaderDate();
