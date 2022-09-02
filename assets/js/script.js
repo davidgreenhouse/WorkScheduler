908bd14ed131da30bfd67ad07b9add6cd3e88148
David Greenhouse <dgreenhouse96@gmail.com>
2022-09-01 20:44:21 -0500

    updated html and js files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,57 @@
 
+
+
 function getHeaderDate() {
+    var currentHeaderDate = moment().format('dddd, MMMM Do');
+    $("#currentDay").text(currentHeaderDate);
+}
+var momentTime = moment().startOf("day").add(7, "hour");
+var hour = moment().format("HH");
+
+
+for(var i = 8; i < 18; i++) {
+    var timeSlot = momentTime.add(1, "hour").format("HH:mm A");
+    var currentState;
+
+    if (hour == i) {
+        currentState = 'present';
+    } else if (hour < i) {
+        currentState = 'future';
+    } else if (hour > i) {
+        currentState = 'past';
+    }
+
+    var newStuff =
+
+    `<container class="row" id='hour-${i}'>
+            <div class="col-2"></div>
+            <div class="hour w-25 p-4 col-1">${timeSlot}</div>
+            <textarea class="description w-50 p-4 col-6 ${currentState} hour-${i}"></textarea>
+            <button class="saveBtn w-25 p-4 col-1 fas fa-save fa-2x"></button>
+            <div class="col-2">
+            </div>  
+        </container>
+        <p></p>`
+        ;
+
+    $(".container").append(newStuff);
+
+};
+
+
+getHeaderDate();
+
+   $(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+   });
+
+   for (var i = 8; i < 18; i++) {
+    $(`.hour-${i}`).val(localStorage.getItem(`hour-${i}`));
+}
+/* function getHeaderDate() {
     var currentHeaderDate = moment().format('dddd, MMMM Do');
     $("#currentDay").text(currentHeaderDate);
 }
@@ -22,11 +74,11 @@ for(var i = 8; i < 18; i++) {
 {/* <container class="" id='hour{}'>
             <div class="col-2"></div>
             <div class="hour w-25 p-4">${}</div>
-        </container> */}
+        </container> */
 
 
 
-    var newStuff =
+/*     var newStuff =
 
     `<container class="row" id='hour-${i}'>
             <div class="col-2"></div>
@@ -42,6 +94,7 @@ for(var i = 8; i < 18; i++) {
     $(".container").append(newStuff);
 
 };
+ */
 
 /* 
 getHeaderDate();
@@ -52,3 +105,15 @@ getHeaderDate();
    for (var i = 8; i < 18; i++) {
     $(`${i}`).val(.getItem(`${i}`));
 } */
+
+/* getHeaderDate();
+   $(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+   });
+
+   for (var i = 8; i < 18; i++) {
+    $(`.hour-${i}`).val(localStorage.getItem(`hour-${i}`));
+} */ 
